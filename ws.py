88f6770c8fd1a7bf9976465a8eacd8f05d383da8
ibrahiml992 <119806467+ibrahiml992@users.noreply.github.com>
2023-03-03 12:01:45 +0530

    Update ws.py

--- ws.py
+++ ws.py
@@ -1,10 +1,7 @@
 # Demonstration of AR operators
-a=float(input("Enter the value of a:"))
-b=float(input("Enter the value of b:"))
+try:
 
-
-print("*"*65)
-
-print("\t a and b power to 2 is {},{}={}".format(a,b,round(((a+b)**2),2)))
-
-print("*"*65)
+  print("sum is 25")
+  print("*"*65)
+except EOFError:
+  print("Please rectify the Error")
